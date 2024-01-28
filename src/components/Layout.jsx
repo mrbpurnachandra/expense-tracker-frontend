da7c8ab49f28e63b610c4d49c0e8513acad9bb90
Prakash Bhattarai <lightvelocity999@gmail.com>
2024-01-28 18:01:34 +0545

    Added margin to allow lower space when there are expense items

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
     return (
-        <div className='max-w-2xl m-auto'>
+        <div className='max-w-2xl m-auto mb-8'>
             <Outlet />
         </div>
     )
